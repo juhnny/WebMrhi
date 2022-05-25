4982c902605db9e50df257dea4d4d75fdd1b2b7c
juhnny <iwibest@naver.com>
2022-05-25 17:05:38 +0900

    update

--- Day12JS/worker.js
+++ Day12JS/worker.js
@@ -1,7 +1,7 @@
 // var n = 0
 // while(true){
 //     n++ //반복문이 무한히 돌아가는 상황
-//     document.getElementById('hh').innerHTML = //동작 안함
+//     document.getElementById('hh').innerHTML = //동작 안함. 왜?
 //     //워커는 UI 변경작업 불가(요소를 만들 수 없음)
 
 //     //값을 변경하고 싶다면 main thread 쪽으로 출력하고 싶은 값을 전달
