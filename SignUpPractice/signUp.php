45c89d6c240a77c4ce84e10526353d08ba0d969d
juhnny <iwibest@naver.com>
2022-06-08 14:16:20 +0900

    update signup practice

--- SignUpPractice/signUp.php
+++ SignUpPractice/signUp.php
@@ -16,24 +16,38 @@
     $phoneNum3 = $_POST['phoneNum3'];
     $phoneNum = "$phoneNum1-$phoneNum2-$phoneNum3";
     
+    //유효성 검사 생략
+
+    //중복 검사
     $db = mysqli_connect("localhost", "iwibest", "de2154178!", "iwibest");
     $query = "SELECT * FROM signup_practice WHERE user_id = '$userId'";
     $result = mysqli_query($db, $query);
     if($result){
         $numRow = mysqli_num_rows($result);
         if($numRow > 0) {
-            echo "중복된 아이디입니다.";
+            echo "중복된 아이디입니다.<br><br>";
             return;
         }
     }
 
-    $query = "INSERT INTO signup_practice(user_id) VALUES ('$userId')";
-    // $query = "INSERT INTO dd_topic(topic_name, topic_last_post_datetime) 
-    // VALUES ('$topic', '$now')
-    // ON DUPLICATE KEY UPDATE topic_last_post_datetime = '$now'"; 
+    //비밀번호 암호화 생략
+
+    //DB에 저장
+    $query = "INSERT INTO signup_practice(user_id, user_pw, user_name, user_email, user_zipcode, user_address1, user_address2, user_phone) 
+    VALUES ('$userId', '$pw', '$userName', '$email', '$zipCode', '$address1', '$address2', '$phoneNum')";
+
     $result = mysqli_query($db, $query);
     if($result){
-        echo "<h1>$userId</h1> 저장 완료";
+        echo "저장 완료";
+
+        $query = "SELECT user_no, user_id FROM signup_practice WHERE user_id = '$userId'";
+        $result = mysqli_query($db, $query);
+        if($result){
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $no = $row['user_no'];
+            $id = $row['user_id'];
+            echo "저장된 회원번호: $no, 아이디: $id";
+        }
     } else {
         echo "저장 실패";
     }
